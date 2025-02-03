01af4c31b3db0d588a40e335597f7e6f2fed5686
OlhaVacik <olhavacik@gmail.com>
2025-02-02 18:09:57 -0700

    Added home-work-4

---
+++ home-work-4/loop.js
@@ -0,0 +1,15 @@
+function loop() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+};
+
+loop();
+
+function loop1() {
+    for (let j = 100; j >= 0; j -= 10) {
+        console.log(j);
+    }
+};
+
+loop1();
