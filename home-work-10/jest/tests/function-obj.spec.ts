00d3a2f15f5729d9f4bdd8c019119aea26046aaa
OlhaVacik <olhavacik@gmail.com>
2025-02-27 10:55:07 -0700

    Separation of blocks/tests/steps by new line using Arrange-Act-Assert block

--- home-work-10/jest/tests/function-obj.spec.ts
+++ home-work-10/jest/tests/function-obj.spec.ts
@@ -6,18 +6,23 @@ import { HeartRateZones} from '../src/class-zone';
 
 describe('Jest test suite: printAllFormula', () => {
     it('Should return array with correct strings for AthletePerson', () => {
+
         const formula = new AthletePerson('Bob', 32, 55, 'swimming', 'gelish');
         const expectedOutput = [
             `Your HRmax pulse: ${formula.gelishHRmax()} Using Gelish Formula`,
             `Your HRmax pulse: ${formula.standardHRmax()} Using Standard Formula`,
             `Your HRmax pulse: ${formula.tanakaHRmax()} Using Tanaka Formula`
         ];
+
         const result = printAllFormula(formula);
 
         expect(result).toEqual(expectedOutput);
     });
+
     it('Should return array from three elements', () => {
+
         const formula = new AthletePerson('Bob', 32, 55, 'swimming', 'gelish');
+
         const result = printAllFormula(formula);
 
         expect(result.length).toBe(3);
@@ -26,8 +31,10 @@ describe('Jest test suite: printAllFormula', () => {
 
 describe('Jest test suite: printReserveRate', () => {
     it('Should return string with correct reserve rate', () => {
+
         const athlete = new AthletePerson('Bob', 32, 55, 'swimming', 'gelish');
         const expectedOutput = `Hi ${athlete.name}, Your reserve ${athlete.getHeartRateReserve()}.`;
+
         const result = printReserveRate(athlete);
 
         expect(result).toEqual(expectedOutput);
@@ -36,8 +43,10 @@ describe('Jest test suite: printReserveRate', () => {
 
 describe('Jest unit test: printHRmax', () => {
     it('Should return string with correct max rate', () => {
+
         const athlete = new AthletePerson('Tom', 22, 65, 'running', 'gelish');
         const expectedOutput = `You are ${athlete.age} Don't exceed ${athlete.maxHR()}.`;
+
         const result = printHRmax(athlete);
 
         expect(result).toEqual(expectedOutput);
@@ -46,6 +55,7 @@ describe('Jest unit test: printHRmax', () => {
 
 describe('Jest unit test: printZones', () => {
     it('Should return array with correct pulses zones', () => {
+
         const zone = new HeartRateZones('Jane', 45, 70);
         const expectedOutput = [
             `The pulse zone 1: ${zone.zone1}. Light activity zone.`,
@@ -54,6 +64,7 @@ describe('Jest unit test: printZones', () => {
             `The pulse zone 4: ${zone.zone4}. Anaerobic zone.`,
             `The pulse zone 5: ${zone.zone5}. Maximum load zone.`
         ];
+
         const result = printZones(zone);
 
         expect(result).toEqual(expectedOutput);
